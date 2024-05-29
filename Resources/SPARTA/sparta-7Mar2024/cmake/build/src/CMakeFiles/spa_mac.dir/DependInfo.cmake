8c36f1f02d17c2123c46d8a2f9d2fe20fa24a2ac
Prad-R <pradl12121@gmail.com>
2024-05-29 17:38:52 +0530

    Added-sparta

---
+++ Resources/SPARTA/sparta-7Mar2024/cmake/build/src/CMakeFiles/spa_mac.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/prad/Desktop/IITM_Courses/Projects/SRFP_2024_Project/Resources/SPARTA/sparta-7Mar2024/src/main.cpp" "src/CMakeFiles/spa_mac.dir/main.cpp.o" "gcc" "src/CMakeFiles/spa_mac.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/prad/Desktop/IITM_Courses/Projects/SRFP_2024_Project/Resources/SPARTA/sparta-7Mar2024/cmake/build/src/CMakeFiles/sparta_mac.dir/DependInfo.cmake"
+  "/home/prad/Desktop/IITM_Courses/Projects/SRFP_2024_Project/Resources/SPARTA/sparta-7Mar2024/cmake/build/src/FFT/CMakeFiles/pkg_fft.dir/DependInfo.cmake"
+  "/home/prad/Desktop/IITM_Courses/Projects/SRFP_2024_Project/Resources/SPARTA/sparta-7Mar2024/cmake/build/src/STUBS/CMakeFiles/pkg_mpi_stubs.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
