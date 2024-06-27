8d703d7337bc33997545681c624122375f091955
Prad-R <pradl12121@gmail.com>
2024-06-27 13:42:30 +0530

    Added MATLAB code for plotting the data : 27.06.2024

--- Resources/SPARTA/sparta-7Mar2024/my_sims/1d_fourier_flow/saved_sims/kn_0.1_____20ppgc_____0.01sec/Data_Plotter.m
+++ Resources/SPARTA/sparta-7Mar2024/my_sims/1d_fourier_flow/saved_sims/kn_0.1_____20ppgc_____0.01sec/Data_Plotter.m
@@ -5,8 +5,11 @@ close all;
 particles = csvread("Particles.csv");
 temperature = csvread("Temperature.csv");
 time_step = 1.0e-8;
-step_freq = 1.0e5;
+step_freq = 5.0e3;
 time = 0;
+tc = 223.15;
+th = 323.15;
+m = (th - tc) / 100;
 
 index = [1 : 1 : 100];
 
@@ -33,11 +36,12 @@ for i = 1 : size(particles,1)
     hold on;
     box on;
     bar(index,temperature(i,:),0.4);
+    plot(index,(tc + m * index),LineWidth=2)
     ylim([200,400]);
     xlabel('Grid Cell $\longrightarrow$',Interpreter='latex',FontSize=15);
     ylabel('Temperature (in K) $\longrightarrow$',Interpreter='latex',FontSize=15);
     title(sprintf('\\textbf{Temperature (in K) at \\boldmath$t = %f$ s}',time),Interpreter='latex',FontSize=18);
-    legend({'Temperature'},Interpreter='latex',FontSize=12);
+    legend({'Temperature','Analytical Solution'},Interpreter='latex',FontSize=12);
     hold off;
     saveas(gcf,sprintf('Plots/Temperature/Temperature_%f_sec.svg',time),'svg');
 end
